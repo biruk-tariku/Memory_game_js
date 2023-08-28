fe44c50afb9ee165a634094353ee272a4dbc511c
biruk tariku <birukyetariku@gmail.com>
2023-08-28 23:32:39 +0300

    Memory Game js

---
+++ js.js
@@ -0,0 +1,3 @@
+console.log(document.head.innerHTML)
+
+document.getElementsByClassName("tiles").style.color="#fff"
---
+++ main.js
@@ -0,0 +1,78 @@
+const tilesContainer = document.querySelector(".tiles");
+const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
+const colorsPicklist = [...colors, ...colors];
+const tileCount = colorsPicklist.length;
+
+
+// Game state
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndofMove = false;
+
+function buildTile(color) {
+    const element = document.createElement("div");
+
+    element.classList.add("tile");
+    element.setAttribute("data-color", color);
+    element.setAttribute("data-revealed", "false");
+
+    element.addEventListener("click", () => {
+        const revealed = element.getAttribute("data-revealed");
+        if (
+            awaitingEndofMove
+            || revealed === "true"
+            || element === activeTile
+        ) {
+            return;
+        }
+        element.style.backgroundColor = color;
+
+        if (!activeTile){
+            activeTile = element;
+            return;
+        }
+
+        const colorToMatch = activeTile.getAttribute("data-color");
+
+        if (colorToMatch === color) {
+            activeTile.setAttribute("data-revealed", "true");
+            element.setAttribute("data-revealed", "true");
+
+            awaitingEndofMove = false;
+            activeTile = null
+            revealedCount +=2;
+
+            
+            if (revealedCount === tileCount){
+                alert("you win! Refresh to play again,");
+            }
+
+            return;
+        }
+
+        awaitingEndofMove = true;
+
+        setTimeout(() => {
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+
+            awaitingEndofMove = false;
+            activeTile = null;
+        }, 1000);
+    });
+
+    return element;
+
+}
+
+for (let i=0; i < tileCount; i++ ) {
+    const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
+    const color = colorsPicklist[randomIndex];
+    const tile = buildTile(color)
+
+    colorsPicklist.splice(randomIndex, 1);
+    tilesContainer.appendChild(tile);
+
+
+    console.log(color);
+}
